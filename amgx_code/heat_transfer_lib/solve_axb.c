d81fcf41ac7fcffef3008d1706f21f748b3becae
divyaprakash-iitd <amz208461@am.iitd.ac.in>
2021-04-24 13:23:18 +0530

    Added a file to test custom amgx library

---
+++ amgx_code/heat_transfer_lib/solve_axb.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include "amgaxb_headers.h"
+
+int main () 
+{
+    double data[] = {-2, 1, 1, -2, 1, 1, -2, 1, 1, -2 };
+    int col_ind[] = {0, 1, 0, 1, 2, 1, 2, 3, 2, 3};
+    int row_ptr[] = {0, 2, 5, 8, 10}; 
+
+    double rhs[] = {-300, 0, 0, -100};
+    double x[] = {0, 0, 0, 0};    
+
+    solveAMG(data, col_ind, row_ptr, rhs, x) 
+}
