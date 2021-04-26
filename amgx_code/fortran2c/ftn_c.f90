2e2ab822fb497985a4e400ff14a1e3b4fb81680f
divyaprakash-iitd <amz208461@am.iitd.ac.in>
2021-04-26 08:13:51 +0530

    Added the wrapper for fortran to c

---
+++ amgx_code/fortran2c/ftn_c.f90
@@ -0,0 +1,13 @@
+module ftn_c
+    interface
+        integer (c_int) function solveAMG(dataM, col_ind, row_ptr, rhs, sol) bind(c, name='solveAMG')
+            use iso_c_binding
+            implicit none
+            type (c_ptr), value :: dataM
+            type (c_ptr), value :: col_ind
+            type (c_ptr), value :: row_ptr
+            type (c_ptr), value :: rhs
+            type (c_ptr), value :: sol
+        end function solveAMG
+    end interface    
+end module ftn_c
