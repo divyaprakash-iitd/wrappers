ab96acc7028eadac76aa2e293bb17435ecd475c5
divyaprakash-iitd <amz208461@am.iitd.ac.in>
2021-04-26 08:10:52 +0530

    Changed the return type of the function solveAMG from void to int

--- amgx_code/heat_transfer_lib/amgaxb_headers.h
+++ amgx_code/heat_transfer_lib/amgaxb_headers.h
@@ -1,3 +1,3 @@
 void errAndExit(const char *err);
 void print_callback(const char *msg, int length);
-void solveAMG(double data[], int col_ind[], int row_ptr[], double rhs[], double *sol);
+int solveAMG(double data[], int col_ind[], int row_ptr[], double rhs[], double *sol);
