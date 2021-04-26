7475c43589332cd636b66ad00f94867bc52f74e3
divyaprakash-iitd <amz208461@am.iitd.ac.in>
2021-04-26 11:08:54 +0530

    changed case of variables

--- amgx_code/fortran2c/ftn_c.f90
+++ amgx_code/fortran2c/ftn_c.f90
@@ -1,13 +1,13 @@
 module ftn_c
     interface
-        integer (c_int) function solveamg(dataM, col_ind, row_ptr, rhs, sol) bind(c, name='solveamg')
+        integer (c_int) function solveamg(datam, col_ind, row_ptr, rhs, sol) bind(c, name='solveamg')
             use iso_c_binding
             implicit none
-            type (c_ptr), value :: dataM
+            type (c_ptr), value :: datam
             type (c_ptr), value :: col_ind
             type (c_ptr), value :: row_ptr
             type (c_ptr), value :: rhs
             type (c_ptr), value :: sol
-        end function solveAMG
+        end function solveamg
     end interface    
 end module ftn_c
--- amgx_code/fortran2c/solveaxb.f90
+++ amgx_code/fortran2c/solveaxb.f90
@@ -4,22 +4,22 @@ program solveaxb
     !implicit none
     ! Declare variables
     integer :: nnz, col, row, b_size
-    real (c_double), dimension(:), allocatable, target :: mtx, rhs, sol
+    real (c_double), dimension(:), allocatable, target :: datam, rhs, sol
     integer (c_int), dimension(:), allocatable, target :: col_ind, row_ptr
 
     nnz = 10; col = 10; row = 5; b_size = 4;
-    allocate(mtx(nnz),col_ind(col),row_ptr(row),rhs(b_size),sol(b_size))
+    allocate(datam(nnz),col_ind(col),row_ptr(row),rhs(b_size),sol(b_size))
 
-    mtx = (/-2, 1, 1, -2, 1, 1, -2, 1, 1, -2/)
+    datam = (/-2, 1, 1, -2, 1, 1, -2, 1, 1, -2/)
     col_ind = (/0, 1, 0, 1, 2, 1, 2, 3, 2, 3/)
     row_ptr = (/0, 2, 5, 8, 10/)
 
     rhs = (/-300, 0, 0, -100/)
     sol = (/-300, 0, 0, -100/)
     
-    call solveamg(c_loc(mtx), c_loc(col_ind), c_loc(row_ptr),c_loc(rhs),c_loc(sol))  
+    call solveamg(c_loc(datam), c_loc(col_ind), c_loc(row_ptr),c_loc(rhs),c_loc(sol))  
  
-   ! print *, mtx
+   ! print *, datam
    ! print *, col_ind
    ! print *, row_ptr
    ! print *, rhs
