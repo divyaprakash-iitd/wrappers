bbc09f4502f7fdd89a21630e3faffc85ce57b441
divyaprakash-iitd <amz208461@am.iitd.ac.in>
2021-07-18 13:25:03 +0530

    Changed the function definition in the header file

--- amgx_code/axb_amgx/axb_amgx_headers.h
+++ amgx_code/axb_amgx/axb_amgx_headers.h
@@ -1,3 +1,3 @@
 int initialize_amgx(int *crs_data, double *data, int *col_ind, int *row_ptr, double *rhs, double *sol); 
-int solveamg(int *crs_data, double *rhs, double *sol);
+int solveamg(double *rhs, double *sol);
 int destroy_amgx(); 
